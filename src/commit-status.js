e4407a26a64bf079e568d72bc526bfd8bfdd7452
Shawn Allen <shawn.allen@github.com>
2019-02-01 00:33:51 -0800

    chore: nix token length console.warn()

--- src/commit-status.js
+++ src/commit-status.js
@@ -2,7 +2,6 @@ const {post} = require('commit-status')
 
 module.exports = function commitStatus(host) {
   const {GITHUB_ACTION = 'deploy', GITHUB_REPOSITORY, GITHUB_SHA, GITHUB_TOKEN = ''} = process.env
-  console.warn(`[deploy] token length: ${GITHUB_TOKEN.length}`)
   const [owner, repo] = GITHUB_REPOSITORY.split('/')
   return post({
     token: GITHUB_TOKEN,
